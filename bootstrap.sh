6e60d4b2a556c530e41e3b2a20eeb8a3c791df9c
Anton Iakimov <yakimant@gmail.com>
2025-02-03 12:24:11 +0100

    bootstrap: add few links

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-ln -fs $HOME/work/dotfiles/.alacritty.toml $HOME/.alacritty.toml
-ln -fs $HOME/work/dotfiles/.tmux.conf $HOME/.tmux.conf
-ln -fs $HOME/work/dotfiles/.zshrc $HOME/.zshrc
-ln -fs $HOME/work/dotfiles/.zsh $HOME/.zsh
-ln -fs $HOME/work/dotfiles/.gitconfig $HOME/.gitconfig
-ln -fs $HOME/work/dotfiles/.gitignore $HOME/.gitignore
+ln -fvs $HOME/work/dotfiles/.alacritty.toml $HOME/.alacritty.toml
+ln -fvs $HOME/work/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -fvs $HOME/work/dotfiles/.zshrc $HOME/.zshrc
+ln -fvs $HOME/work/dotfiles/.zsh $HOME/.zsh
+ln -fvs $HOME/work/dotfiles/.gitconfig $HOME/.gitconfig
+ln -fvs $HOME/work/dotfiles/.gitignore $HOME/.gitignore
 
 mkdir -p $HOME/.config
-ln -fs $HOME/work/dotfiles/.config/nvim $HOME/.config/nvim
+ln -fvs $HOME/work/dotfiles/.config/nvim $HOME/.config/nvim
 
-ln -fs $HOME/work/secrets $HOME/.passage/store
+rm -i $HOME/.passage/store
+ln -fvs $HOME/work/secrets $HOME/.passage/store
+
+ln -fvs /Volumes/supersecrets $HOME/work/supersecrets
 
 #./.macos
 
-ln -fs $HOME/.ssh/id_ed25519_sk_no_touch_24553389 $HOME/.ssh/id_ed25519_sk_no_touch
-ln -fs $HOME/.ssh/id_ed25519_sk_no_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_no_touch.pub
-ln -fs $HOME/.ssh/id_ed25519_sk_touch_24553389 $HOME/.ssh/id_ed25519_sk_touch
-ln -fs $HOME/.ssh/id_ed25519_sk_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_touch.pub
+ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389 $HOME/.ssh/id_ed25519_sk_no_touch
+ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_no_touch.pub
+ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389 $HOME/.ssh/id_ed25519_sk_touch
+ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_touch.pub
