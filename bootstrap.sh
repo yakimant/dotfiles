fb00f52c808a02df65abaa46bbcdf36cb9923e0c
Anton Iakimov <yakimant@gmail.com>
2025-03-17 15:49:50 +0100

    bin: init

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,17 +7,26 @@ ln -fvs $HOME/work/dotfiles/.zsh $HOME/.zsh
 ln -fvs $HOME/work/dotfiles/.gitconfig $HOME/.gitconfig
 ln -fvs $HOME/work/dotfiles/.gitignore $HOME/.gitignore
 
+mkdir -p $HOME/bin
+ln -fvs $HOME/work/dotfiles/bin/* $HOME/bin/
+
 mkdir -p $HOME/.config
 ln -fvs $HOME/work/dotfiles/.config/nvim $HOME/.config/nvim
 
-rm -i $HOME/.passage/store
-ln -fvs $HOME/work/secrets $HOME/.passage/store
+#rm -i $HOME/.passage/store
+#ln -fvs $HOME/work/secrets $HOME/.passage/store
 
-ln -fvs /Volumes/supersecrets $HOME/work/supersecrets
+#ln -fvs /Volumes/supersecrets $HOME/work/supersecrets
 
 #./.macos
 
-ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389 $HOME/.ssh/id_ed25519_sk_no_touch
-ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_no_touch.pub
-ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389 $HOME/.ssh/id_ed25519_sk_touch
-ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_touch.pub
+#ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389 $HOME/.ssh/id_ed25519_sk_no_touch
+#ln -fvs $HOME/.ssh/id_ed25519_sk_no_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_no_touch.pub
+#ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389 $HOME/.ssh/id_ed25519_sk_touch
+#ln -fvs $HOME/.ssh/id_ed25519_sk_touch_24553389.pub $HOME/.ssh/id_ed25519_sk_touch.pub
+
+# setup user services
+#yabai --install-service
+#yabai --start-service
+#skhd --install-service
+#skhd --start-service
